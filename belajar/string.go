dc981d4b53803566f7823e972c1b59601c5b1c21
fadil dlz <fadil.dlz@gmail.com>
2020-12-21 10:36:03 +0700

    first 01-05

---
+++ belajar/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("fadil")
+	fmt.Println("fadildlz")
+	fmt.Println("fadillah nursyahiddin")
+
+	fmt.Println(len("fadil"))
+	fmt.Println("fadildlz"[0])
+	fmt.Println("fadillah nursyahiddin"[1])
+}
